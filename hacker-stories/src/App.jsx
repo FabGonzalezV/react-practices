cc215bc0fae2e4ad9f58f7924b6dde12ba32ad53
FabGonzalezV <fabian.gonzalez.valentin@gmail.com>
2023-06-15 11:10:06 -0600

    nested destructuring

--- hacker-stories/src/App.jsx
+++ hacker-stories/src/App.jsx
@@ -69,14 +69,23 @@ const List = ({ list }) => (
     ))}
   </ul>
 );
-const Item = ({ item }) => (
+//nested destructuring
+const Item = ({
+  item: {
+    title,
+    url,
+    author,
+    num_comments,
+    points,
+  },
+}) => (
   <li>
     <span>
-      <a href={item.url}>{item.title}</a>
+      <a href={url}>{title}</a>
     </span>
-    <span>{item.author}</span>
-    <span>{item.num_comments}</span>
-    <span>{item.points}</span>
+    <span>{author}</span>
+    <span>{num_comments}</span>
+    <span>{points}</span>
   </li>
 );
 
