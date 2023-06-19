ac0887f2ca9dca92e35b58098209db1837f69031
FabGonzalezV <fabian.gonzalez.valentin@gmail.com>
2023-06-19 12:49:25 -0600

    react conditional rendering

--- hacker-stories/src/App.jsx
+++ hacker-stories/src/App.jsx
@@ -48,12 +48,14 @@ function App() {
     'React');
 
   const [stories, setStories] = React.useState([]);
-
-  
+  const [isLoading, setIsLoading] = React.useState(false);
+  const [isError, setIsError] = React.useState(false);
   React.useEffect(() => {
+    setIsLoading(true);
     getAsyncStories().then(result => {
       setStories(result.data.stories);
-    });
+      setIsLoading(false);
+    }).catch(() => setIsError(true));
   }, []);
   const handleRemoveStory = (item) => {
     const newStories = stories.filter(
@@ -92,7 +94,15 @@ function App() {
       >
         <strong>Search:</strong>
       </InputWithLabel>
-      <List list={searchedStories} onRemoveItem={handleRemoveStory} />
+      {isError && <p>Something went wrong ...</p>}
+      {isLoading ? (
+        <p>Loading ...</p>
+      ) : (
+        <List
+          list={searchedStories}
+          onRemoveItem={handleRemoveStory}
+        />
+      )}
     </div>
 
 
